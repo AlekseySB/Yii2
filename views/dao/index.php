75eea8ddd8a8837a77b37f20f1fda559932032ef
AlekseySB <abilyakov@yandex.ru>
2019-08-15 11:42:13 +0300

    L4

---
+++ views/dao/index.php
@@ -0,0 +1,56 @@
+<?php
+
+
+?>
+
+<div class="row">
+    <div class="col-md-6">
+        <pre>
+            <?php
+                print_r($users);
+            ?>
+        </pre>
+    </div>
+    <div class="col-md-6">
+        <pre>
+            <?php
+                print_r($activities);
+            ?>
+        </pre>
+    </div>
+    <div class="col-md-6">
+        <pre>
+            <?php
+                print_r($firstActivity);
+            ?>
+        </pre>
+    </div>
+    <div class="col-md-6">
+        <pre>
+            <?php
+                print_r($count);
+            ?>
+        </pre>
+    </div>
+    <div class="col-md-6">
+        <pre>
+            <?php
+                print_r($auth);
+            ?>
+        </pre>
+    </div>
+    <div class="col-md-6">
+        <pre>
+            <?php
+                print_r($email);
+            ?>
+        </pre>
+    </div>
+    <div class="col-md-6">
+        <pre>
+            <?php
+                print_r($userActivities);
+            ?>
+        </pre>
+    </div>
+</div>
